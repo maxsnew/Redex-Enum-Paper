99031af43af105e241b149b89302596654d2b0e8
Robby Findler <robby@racket-lang.org>
2014-03-17 22:19:54 -0500

    adjust the isabelle-authors-conjecture to do 1000 attempts in quick check (to match redex) and then run it in a loop and report the results from isabelle-authors-conjecture.rkt
    current script output is:
    
    redex
    0 failures
    1000 successes
    5.036 average attempts for each success
    
    quickcheck
    11 failures
    989 successes
    3.129423660262892 average attempts for each success

--- isabelle-authors-conjecture.hs
+++ isabelle-authors-conjecture.hs
@@ -2,17 +2,25 @@ module Main where
 
 {- To run:
 $ cabal install quickcheck # if you don't have it
+
+# and then either:
 $ runhaskell isabelle-authors-conjecture
+
+# or
+$ ghc isabelle-authors-conjecture.hs
+$ ./isabelle-authors-conjecture
+
 -}
 
 import Test.QuickCheck
 
 main :: IO ()
-main = quickCheck (prop_append :: [Int] -> [Int] -> Int -> Bool)
+main = quickCheckWith stdArgs {maxSuccess=100} prop_append
 
 safeBang :: [a] -> Int -> Maybe a
 safeBang xs n = if (n >= 0) && (n < length xs)
                 then Just $ xs !! n
                 else Nothing
                      
+prop_append :: [Int] -> [Int] -> Int -> Bool
 prop_append xs ys n = ((xs ++ ys) `safeBang` (length xs + n)) == (xs `safeBang` n)
